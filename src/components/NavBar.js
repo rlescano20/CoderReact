39bb28c481ff7c04b538a49bf66cfc28887fa195
rlescano20 <rodrilescano@gmail.com>
2021-06-15 22:55:16 -0300

    NavBar Creation

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+    return (
+          <div>
+           <h1>Menu</h1>      
+        <nav>
+        <a href="#Opc1">Opc1</a>|
+        <a href="#Opc2"> Opc2 </a>|
+        <a href="#Opc3">Opc3</a>
+        </nav>
+        </div>       
+     )
+}
